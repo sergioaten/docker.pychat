6c74e7b2f631592aa657b1e1352e2e37b3483546
Sergio <sergio@git.com>
2023-05-26 01:08:52 +0200

    working

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
+# Import necessary modules
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
+# Create Flask app instance
+app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'mysecretkey'
+
+# Create SocketIO instance and associate it with the app
 socketio = SocketIO(app, async_mode='eventlet')
 
+# Initialize empty dictionaries to store users and messages
 users = {}
 messages = []
 
+# Define route for the root URL
 @app.route('/')
 def index():
+    # Render the 'index.html' template
     return render_template('index.html')
 
+# Event handler for client connect event
 @socketio.on('connect')
 def handle_connect():
+    # Emit the 'chat_history' event and send the list of messages to the client
     emit('chat_history', messages)
 
+# Event handler for incoming message event
 @socketio.on('message')
 def handle_message(data):
+    # Get the name of the sender from the users dictionary using the session ID
     name = users[request.sid] if request.sid in users else 'Anónimo'
+    
+    # Create a message dictionary with the sender's name and the message content
     message = {'name': name, 'message': data['message']}
+    
+    # Add the message to the messages list
     messages.append(message)
+    
+    # Emit the 'message' event and broadcast it to all connected clients
     emit('message', message, broadcast=True)
 
+# Event handler for setting the user's name
 @socketio.on('set_name')
 def set_name(name):
+    # Store the user's name in the users dictionary with the session ID as the key
     users[request.sid] = name
 
+# Run the app when the script is executed directly
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug="TRUE")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
---
+++ static/script.js
@@ -0,0 +1,150 @@
+$(document).ready(function() {
+    var socket = io.connect('http://localhost:5000');
+    var name = '';
+    var errorVisible = false;
+  
+    // Scroll to the bottom of the messages list
+    function scrollToBottom() {
+      var messagesList = document.getElementById('messages');
+      messagesList.scrollTop = messagesList.scrollHeight;
+    }
+  
+    // Show the name input form and hide the chat form
+    function showNameForm() {
+      $('#name-form').show();
+      $('#chat-form').hide();
+    }
+  
+    // Show the chat form and scroll to the bottom of the messages list
+    function showChatForm() {
+      $('#name-form').hide();
+      $('#chat-form').show();
+      scrollToBottom();
+    }
+  
+    // Display the chat history
+    function showChatHistory(messages) {
+      var messagesContainer = $('#messages'); // Clear previous messages
+      
+      if (messages.length > 0) {
+        $('#no-messages').hide(); // Hide the "No messages yet" message
+        
+        for (var i = 0; i < messages.length; i++) {
+          var message = messages[i].message;
+          var sender = messages[i].name;
+          messagesContainer.append('<li><strong>' + sender + ':</strong> ' + message + '</li>');
+        }
+        
+        // Scroll to the last message
+        messagesContainer.scrollTop(messagesContainer.prop("scrollHeight"));
+      } else {
+        $('#no-messages').show(); // Show the "No messages yet" message
+      }
+      
+      scrollToBottom();
+    }
+  
+    // Display an error message
+    function displayErrorMessage(message) {
+      $('#error-message').text(message);
+      $('#error-message').show();
+      errorVisible = true;
+    }
+  
+    // Hide the error message
+    function hideErrorMessage() {
+      $('#error-message').hide();
+      errorVisible = false;
+    }
+  
+    // Send a message
+    function sendMessage() {
+      var messageInput = $('#message');
+      var message = messageInput.val().trim();
+  
+      if (message !== '') {
+        socket.emit('message', { 'message': message });
+        messageInput.val('');
+        hideErrorMessage();
+  
+        // Remove the information message
+        $('#no-messages').remove();
+      } else {
+        displayErrorMessage('Please enter a message.');
+      }
+    }
+  
+    // Set the username
+    function setUsername() {
+      var username = $('#name-input').val().trim();
+      if (username !== '') {
+        name = username;
+        socket.emit('set_name', name);
+        showChatForm();
+        if (errorVisible) {
+          hideErrorMessage();
+        }
+      } else {
+        displayErrorMessage('Please enter a username.');
+      }
+    }
+  
+    // Event listener for the name submit button click
+    $('#name-submit').on('click', function() {
+      setUsername();
+    });
+  
+    // Event listener for the name input field keypress
+    $('#name-input').on('keypress', function(event) {
+      if (event.which === 13) {
+        event.preventDefault();
+        setUsername();
+      }
+    });
+  
+    // Event listener for receiving chat history from the server
+    socket.on('chat_history', function(messages) {
+      showChatHistory(messages);
+    });
+  
+    // Event listener for receiving a new message from the server
+    socket.on('message', function(data) {
+      var message = data.message;
+      var sender = data.name;
+      $('#messages').append('<li><strong>' + sender + ':</strong> ' + message + '</li>');
+      scrollToBottom();
+    });
+  
+    // Event listener for the send button click
+    $('#send').on('click', function() {
+      sendMessage();
+    });
+  
+    // Event listener for the message input field keypress
+    $('#message').on('keypress', function(event) {
+      if (event.which === 13) {
+        event.preventDefault();
+        if ($('#message').val().trim() !== '') {
+          sendMessage();
+        } else {
+          if (errorVisible) {
+            hideErrorMessage();
+          }
+          displayErrorMessage('Please enter a message.');
+        }
+      } else {
+        if (errorVisible) {
+          hideErrorMessage();
+        }
+      }
+    });
+  
+    // Event listener for the error message close button click
+    $('#error-close').on('click', function() {
+      hideErrorMessage();
+    });
+  
+    // Show the name input form initially
+    showNameForm();
+  });
+  
